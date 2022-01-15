78740b3d5034280e1458cfdf93b1a9bc511066c4
MrJin <haosheng0211@gmail.com>
2022-01-15 13:54:22 +0800

    fix: 重新啟用,商品不見問題

--- src/content_script/enable.js
+++ src/content_script/enable.js
@@ -5,7 +5,7 @@ import storage from '@/utils/storage'
   if (active && type === 'enable') {
     for (const item of document.querySelectorAll('#list > table > tbody > tr:not(:first-child) > td:first-child')) {
       const id = item.innerText.replace(/[^\d+]/, '')
-      await fetch(`https://www.591.com.tw/index.php?module=furniture&action=optFurniture&post_id=${id}`)
+      await fetch(`https://www.591.com.tw/index.php?module=furniture&action=optFurniture&post_id=${id}&frontPage=index.php%3Fmodule%3DuserCenter%26action%3DexpireList&opt=repost`)
     }
     setTimeout(() => {
       window.location.reload()
